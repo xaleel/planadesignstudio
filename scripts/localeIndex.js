3f9c1eb46221f903ce5251fca0aeda09af4b8ef1
xaleel <xaleelchess@gmail.com>
2024-06-27 16:11:13 +0300

    init

---
+++ scripts/localeIndex.js
@@ -0,0 +1,45 @@
+window.t = {
+	en: {
+		// header
+		home: "Home",
+		about_us: "About Us",
+		projects: "Projects",
+		contact_us: "Contact Us",
+		lang_switch: "العربية",
+		// content
+		hero_1: "Modern",
+		hero_2: "Interior",
+		hero_3: "Design Studio",
+		hero_description:
+			"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias quasi, repellendus, dolorum officiis magnam porro quod fugiat ducimus tempore ex provident error.",
+		explore_more: "Explore More",
+		info_phone_number: "+962 78 9808 404",
+		schedule_consultation: "Schedule a Consultation",
+		cta: "Reimagine your space with PlanA Design Studio!",
+		// footer
+		info_email: "info@planadesignstudio.com",
+		info_address:
+			"Mecca Street, Al Haraneh Complex, Building 150, 3th floor",
+	},
+	ar: {
+		// header
+		home: "الرئيسية",
+		about_us: "من نحن",
+		projects: "مشاريعنا",
+		contact_us: "اتصل بنا",
+		lang_switch: "English",
+		// content
+		hero_1: "استوديو",
+		hero_2: "تصميم",
+		hero_3: "داخلي حديث",
+		hero_description:
+			"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias quasi, repellendus, dolorum officiis magnam porro quod fugiat ducimus tempore ex provident error.",
+		explore_more: "استكشف المزيد",
+		schedule_consultation: "احجز استشارة",
+		cta: "أعد تصور مساحتك مع استوديو تصميم بلان إيه!",
+		// footer
+		info_phone_number: "+962 78 9808 404",
+		info_email: "info@planadesignstudio.com",
+		info_address: "شارع مكة، مجمع الحرانة، بناية 150، الطابق 3",
+	},
+};
