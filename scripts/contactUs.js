ef9939cc4e4b42ec29e728428275e065ba12bed6
xaleel <xaleelchess@gmail.com>
2024-06-27 16:11:59 +0300

    under construction

--- scripts/contactUs.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("DOMContentLoaded", () => {
-	const iframeSrc = {
-		en: "https://docs.google.com/forms/d/e/1FAIpQLSfznMddDUJ505i2wzPSL6MkJQiyCVmVgGwIp4Lp9QKyGBpOTg/viewform?embedded=true",
-		ar: "https://docs.google.com/forms/d/e/1FAIpQLSeej5Rh0JIZu3vnVxwTayr2Nh1SlcxnxmGAWIeoqqA_OpfqDw/viewform?embedded=true",
-	};
-	document.querySelector("iframe").src = iframeSrc[locale];
-});
