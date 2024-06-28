72278cf398571f650d7430f24e17e969ebde7554
xaleel <xaleelchess@gmail.com>
2024-06-28 18:02:54 +0300

    about us

---
+++ scripts/localeAbout.js
@@ -0,0 +1,96 @@
+window.t = {
+	en: {
+		// header
+		home: "Home",
+		about_us: "About Us",
+		projects: "Projects",
+		contact_us: "Contact Us",
+		lang_switch: "العربية",
+		// content
+		about_plan_a: "About Plan A",
+		about_plan_a_1:
+			"Plan A is a full-service creative design studio offering interior design services for a variety of clients in the Middle Eastern region and abroad.",
+		about_plan_a_2:
+			"In its Design practice, the studio fully embraces the idea of design pragmatism, following a unique design in the planning and execution of its projects, remaining open to the client’s demands, while translating their needs into a feasible design scheme, whether it is commercial, residential, or conceptual. Both functional and aesthetic elements are balanced in every project, ensuring that spaces are not only platforms of free expression, but also practical and fully customized for the client.",
+		our_services: "Our Services",
+		our_services_1: "1. INTERIOR DESIGN & EXECUTION",
+		our_services_1_1:
+			"Our Services start from the initial conceptual phase all the way to execution and consultancy.",
+		our_services_2: "2. FURNITURE PRODUCTION",
+		our_services_2_1:
+			"We provide custom made furniture pieces using the finest materials that are carefully manufactured with high quality.",
+		our_services_3: "3. EXTERIOR DESIGN & LANDSCAPE",
+		our_services_3_1:
+			"We offer artistic compositions to emphasize on outdoor areas design, achieving environmental and aesthetic outcomes.",
+		our_services_4: "4. INTERIOR STYLING",
+		our_services_4_1:
+			"We offer artistic compositions to emphasize on outdoor areas design, achieving environmental and aesthetic outcomes.",
+		vision_and_mission: "Vision & Mission",
+		vision_1:
+			"At Plan A, we create designs so exceptional, you'll never need a Plan B",
+		vision_2:
+			"Being a trustworthy and dependable builder and interior designer, striving for perfection.",
+		vision_3:
+			"We focus on delivering exceptional quality and providing the best service to all our customers.",
+		vision_4:
+			"Our dynamic concepts are designed to arouse curiosity and engage the community.",
+		quality: "Quality",
+		quality_desc:
+			"Plan A commits to building and creating masterpieces with perfection in every detail, achieving flawless results without compromise.",
+		dependability: "Dependability",
+		dependability_desc:
+			"Every project will be tailored to the customer's needs and completed on time as promised from the start.",
+		interested: "Interested in working with us?",
+		// footer
+		info_phone_number: "+962 78 9808 404",
+		info_email: "info@planadesignstudio.com",
+		info_address:
+			"Mecca Street, Al Haraneh Complex, Building 150, 3th floor",
+	},
+	ar: {
+		// header
+		home: "الرئيسية",
+		about_us: "من نحن",
+		projects: "مشاريعنا",
+		contact_us: "اتصل بنا",
+		lang_switch: "English",
+		// content
+		about_plan_a: "عن Plan A",
+		about_plan_a_1:
+			"Plan A هو استوديو تصميم إبداعي متكامل الخدمات يقدم خدمات التصميم الداخلي لمجموعة متنوعة من العملاء في منطقة الشرق الأوسط والخارج.",
+		about_plan_a_2:
+			"في ممارساته التصميمية، يتبنى الاستوديو بالكامل فكرة البراجماتية التصميمية، ويتبع تصميمًا فريدًا في التخطيط وتنفيذ مشاريعه، ويظل منفتحًا على مطالب العميل، مع ترجمة احتياجاته إلى مخطط تصميم قابل للتطبيق، سواء كان تجاريًا أو سكنيًا أو مفاهيميًا. يتم تحقيق التوازن بين العناصر الوظيفية والجمالية في كل مشروع، مما يضمن أن المساحات ليست مجرد منصات للتعبير الحر، بل إنها أيضًا عملية ومخصصة بالكامل للعميل.",
+		our_services: "خدماتنا",
+		our_services_1: "1. التصميم الداخلي والتنفيذ",
+		our_services_1_1:
+			"تبدأ خدماتنا من مرحلة التصميم الأولية وحتى التنفيذ والاستشارات.",
+		our_services_2: "2. إنتاج الأثاث",
+		our_services_2_1:
+			"نحن نقدم قطع أثاث مصنوعة حسب الطلب باستخدام أجود المواد التي يتم تصنيعها بعناية عالية الجودة.",
+		our_services_3: "3. التصميم الخارجي والمناظر الطبيعية",
+		our_services_3_1:
+			"نقدم تركيبات فنية للتأكيد على تصميم المناطق الخارجية، وتحقيق نتائج بيئية وجمالية.",
+		our_services_4: "4. التصميم الداخلي",
+		our_services_4_1:
+			"نقدم تركيبات فنية للتأكيد على تصميم المناطق الخارجية، وتحقيق نتائج بيئية وجمالية.",
+		vision_and_mission: "الرؤية والرسالة",
+		vision_1:
+			"في Plan A، نبتكر تصميمات استثنائية للغاية، ولن تحتاج أبدًا إلى Plan B",
+		vision_2:
+			"كوننا شركة بناء ومصممة ديكورات داخلية جديرة بالثقة، نسعى جاهدين لتحقيق الكمال.",
+		vision_3:
+			"نركز على تقديم جودة استثنائية وتوفير أفضل خدمة لجميع عملائنا.",
+		vision_4: "تم تصميم مفاهيمنا الديناميكية لإثارة الفضول وإشراك المجتمع.",
+		quality: "الجودة",
+		quality_desc:
+			"تلتزم شركة Plan A ببناء وإنشاء روائع فنية بإتقان في كل التفاصيل، وتحقيق نتائج خالية من العيوب دون أي تنازلات.",
+		dependability: "الاعتمادية",
+		dependability_desc:
+			"سيتم تصميم كل مشروع وفقًا لاحتياجات العميل وإكماله في الوقت المحدد كما وعدنا منذ البداية.",
+		interested: "هل ترغب في العمل معنا؟",
+		// footer
+		info_phone_number: "+962 78 9808 404",
+		info_email: "info@planadesignstudio.com",
+		info_address: "شارع مكة، مجمع الحرانة، بناية 150، الطابق 3",
+	},
+};
